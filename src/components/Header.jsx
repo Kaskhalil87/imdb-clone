1d785578f69582d1e8ad14a6f4c454c920e87e89
Kaskhalil87 <140751168+Kaskhalil87@users.noreply.github.com>
2024-04-07 15:33:07 -0500

    added header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import Link from "next/link";
+import Menu from "./Menu";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+      <div className="flex gap-4">
+        <Menu title="home" address="/" Icon={AiFillHome} />
+        <Menu title="about" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+      <Link href={"/"} className=" flex gap-1 items-center">
+        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
+          IMDB
+        </span>
+        <span className="text-xl hidden sm:inline">Clone</span>
+      </Link>
+    </div>
+  );
 }
