42756edcd45dea339e7198af19aae69ae7fa4de8
Kaskhalil87 <140751168+Kaskhalil87@users.noreply.github.com>
2024-04-09 16:11:27 -0500

    added dark and light mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Menu from "./Menu";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import DarkMode from "./DarkMode";
 
 export default function Header() {
   return (
@@ -10,12 +11,15 @@ export default function Header() {
         <Menu title="home" address="/" Icon={AiFillHome} />
         <Menu title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <Link href={"/"} className=" flex gap-1 items-center">
-        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
-          IMDB
-        </span>
-        <span className="text-xl hidden sm:inline">Clone</span>
-      </Link>
+      <div className="flex items-center gap-4">
+        <DarkMode />
+        <Link href={"/"} className=" flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
+            IMDB
+          </span>
+          <span className="text-xl hidden sm:inline">Clone</span>
+        </Link>
+      </div>
     </div>
   );
 }
